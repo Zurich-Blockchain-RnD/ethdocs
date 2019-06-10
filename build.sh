7e8345d89d522175ae7cd3f5b3c6c10134c9e67f
Abhi <starktony01>
2019-06-10 13:40:04 +0530

    travis mainpulation

--- build.sh
+++ build.sh
@@ -3,15 +3,15 @@
 setup_git() {
   git config --global user.email "abhi.codes0@gmail.com"
   git config --global user.name "cryptoabhi"
+  git remote add origin https://github.com/ethdocs/ethdocs.github.io
 }
 
 commit_website_files() {
-  git add . *
+  git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin https://github.com/ethdocs/ethdocs.github.io
   git push --force origin master
 }
 
